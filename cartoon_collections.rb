cf45338107e6e4724e1e0fb472b32725f9025b08
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-18 00:08:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,6 +5,7 @@ def greet_characters(array)
   
   array.each do |character|
     puts "Hello #{character}!"
+  end
   
 end
 
@@ -15,5 +16,6 @@ def list_dwarves(array)
   
   array.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
+  end
   
 end
